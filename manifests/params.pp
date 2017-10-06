f48636745a5976e0c201989eeef631b0bcd1e09b
kananiv <vishal.kanani@cdk.com>
2017-10-06 15:57:50 +0530

    Fixing error to make code competible with puppet 2106

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,56 +4,41 @@
 # It sets variables according to platform
 #
 class consul::params {
-  $acls                  = {}
-  $archive_path          = ''
-  $bin_dir               = '/usr/local/bin'
-  $checks                = {}
-  $config_defaults       = {}
-  $config_dir            = '/etc/consul'
-  $config_hash           = {}
-  $config_mode           = '0664'
-  $docker_image          = 'consul'
-  $download_extension    = 'zip'
-  $download_url          = undef
-  $download_url_base     = 'https://releases.hashicorp.com/consul/'
-  $extra_groups          = []
-  $extra_options         = ''
-  $group                 = 'consul'
-  $log_file              = '/var/log/consul'
+
   $install_method        = 'url'
-  $join_wan              = false
-  $manage_group          = true
-  $manage_service        = true
-  $manage_user           = true
-  $package_ensure        = 'latest'
   $package_name          = 'consul'
-  $pretty_config         = false
-  $pretty_config_indent  = 4
-  $proxy_server          = undef
-  $purge_config_dir      = true
-  $restart_on_change     = true
-  $service_enable        = true
-  $service_ensure        = 'running'
-  $services              = {}
-  $ui_download_extension = 'zip'
-  $ui_download_url       = undef
-  $ui_download_url_base  = 'https://releases.hashicorp.com/consul/'
-  $ui_package_ensure     = 'latest'
+  $package_ensure        = 'latest'
+  $download_url_base     = 'https://releases.hashicorp.com/consul/'
+  $download_extension    = 'zip'
   $ui_package_name       = 'consul_ui'
-  $user                  = 'consul'
-  $version               = '0.7.4'
-  $watches               = {}
+  $ui_package_ensure     = 'latest'
+  $ui_download_url_base  = 'https://releases.hashicorp.com/consul/'
+  $ui_download_extension = 'zip'
+  $version               = '0.5.2'
+  $config_mode           = '0660'
 
   case $::architecture {
     'x86_64', 'amd64': { $arch = 'amd64' }
     'i386':            { $arch = '386'   }
     /^arm.*/:          { $arch = 'arm'   }
+    'x64':             { $arch = 'amd64' }
     default:           {
       fail("Unsupported kernel architecture: ${::architecture}")
     }
   }
 
   $os = downcase($::kernel)
+
+  case $::operatingsystem {
+    'windows': {
+      $bin_dir = $::consul_windir
+      $config_dir = "${bin_dir}/config"
+    }
+    default: {
+      $bin_dir = '/usr/local/bin'
+      $config_dir = '/etc/consul'
+    }
+  }
 
   if $::operatingsystem == 'Ubuntu' {
     if versioncmp($::operatingsystemrelease, '8.04') < 1 {
@@ -63,25 +48,17 @@ class consul::params {
     } else {
       $init_style = 'systemd'
     }
-  } elsif $::osfamily == 'RedHat' {
-    case $::operatingsystem {
-      'Fedora': {
-        if versioncmp($::operatingsystemrelease, '12') < 0 {
-          $init_style = 'init'
-        } else {
-          $init_style = 'systemd'
-        }
-      }
-      'Amazon': {
-          $init_style = 'redhat'
-      }
-      default: {
-        if versioncmp($::operatingsystemrelease, '7.0') < 0 {
-          $init_style = 'redhat'
-        } else {
-          $init_style  = 'systemd'
-        }
-      }
+  } elsif $::operatingsystem =~ /Scientific|CentOS|RedHat|OracleLinux/ {
+    if versioncmp($::operatingsystemrelease, '7.0') < 0 {
+      $init_style = 'init'
+    } else {
+      $init_style  = 'systemd'
+    }
+  } elsif $::operatingsystem == 'Fedora' {
+    if versioncmp($::operatingsystemrelease, '12') < 0 {
+      $init_style = 'init'
+    } else {
+      $init_style = 'systemd'
     }
   } elsif $::operatingsystem == 'Debian' {
     if versioncmp($::operatingsystemrelease, '8.0') < 0 {
@@ -101,9 +78,14 @@ class consul::params {
     }
   } elsif $::operatingsystem == 'Darwin' {
     $init_style = 'launchd'
-  } elsif $::operatingsystem == 'FreeBSD' {
-    $init_style = 'freebsd'
+  } elsif $::operatingsystem == 'Amazon' {
+    $init_style = 'init'
+  } elsif $::operatingsystem == 'windows' {
+    $init_style = 'scm'
   } else {
-    fail('Cannot determine init_style, unsupported OS')
+    $init_style = undef
+  }
+  if $init_style == undef {
+    fail('Unsupported OS')
   }
 }
